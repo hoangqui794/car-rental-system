6dcd3ee4f14262e266d9146597fc9a102d854cbb
hoangqui794 <truongqui858@gmail.com>
2026-06-12 23:03:43 +0700

    Enable Swagger in Production

--- CarRental.API/Program.cs
+++ CarRental.API/Program.cs
@@ -82,11 +82,8 @@ builder.Services.AddSwaggerGen(o =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
