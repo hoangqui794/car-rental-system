efbbb8840749549c407c0de42b6c53fa8b81c794
hoangqui794 <truongqui858@gmail.com>
2026-06-16 08:29:15 +0700

    feat: initialize web application configuration with DI, JWT authentication, and Swagger documentation

--- CarRental.API/Program.cs
+++ CarRental.API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173/")
+        policy.WithOrigins("http://localhost:5173")
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials();
